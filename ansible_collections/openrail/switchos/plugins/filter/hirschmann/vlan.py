c5cb5298835588f596b9b2c2481da679dbe9c6f6
Adrian Kailus <adrian.kailus@deutschebahn.com>
2025-06-24 21:46:56 +0200

    feat(roles):  add vlan filter (WIP)

--- ansible_collections/openrail/switchos/plugins/filter/hirschmann/vlan.py
+++ ansible_collections/openrail/switchos/plugins/filter/hirschmann/vlan.py
@@ -1,17 +1,71 @@
+import re
 from ansible.errors import AnsibleFilterError
 
 
 class FilterModule(object):
     def filters(self):
-        return {"parse_vlan_ports_table": self.parse_vlan_ports_table}
+        return {
+            "parse_vlan_ports_table": self.parse_vlan_ports_table,
+            "parse_vlan_ports_table": self.parse_vlans,
+        }
+
+    def parse_vlans(self, stdout_lines):
+        config_text = "\n".join(stdout_lines)
+
+        # Extract the vlan database block
+        m = re.search(
+            r"vlan database(.*?)^\s*exit\s*$", config_text, re.DOTALL | re.MULTILINE
+        )
+        if not m:
+            return []
+        block = m.group(1)
+
+        # Find all vlan add lines
+        vlans = re.findall(r"vlan add (\d+)", block)
+
+        # Find all name lines
+        names = dict(re.findall(r"name (\d+)\s+([^\n]+)", block))
+
+        return [{"vlan_id": vlan, "name": names.get(vlan)} for vlan in vlans]
 
     def parse_vlan_ports_table(self, stdout_lines):
+        """
+
+                vlan database>$vlan add 1337
+                vlan database>$name 1337 leet
+                configure>$interface 1/3-1/7,1/9
+                configure>interface>$vlan participation include 1337
+                $show vlan member current
+
+                VLAN Port membership
+                 Slot: 1
+        VLAN ID  Port: 1234567890123456789012345678
+        -------  ----- ----------------------------
+              1        UUUUUUUUUUUUUUUUUUUUUUUUUUUU
+              2        -T-T-----------------------T
+              8        -T-T-----UUUU----------U---T
+              9        -T-T----U------------------T
+             10        ----------------------------
+            200        -T-TT----------------------T
+            210        -T--TT----------------------
+            218        TTTTTTTTTTTTTTTTTTTTTTTTTTTT
+            219        -T-TTT---------------------T
+            220        -TTTT----------------------T
+
+        Abbreviations:
+        - Not Member
+        T Tagged
+        F Forbidden
+        U Untagged
+
+        """
+
         print("Parsing VLAN ports table...")
         print("Input lines:", stdout_lines)
         # Find the header line with port numbers
         port_line = None
         for line in stdout_lines:
-            if line.strip().startswith("VLAN ID"):
+            if line.strip().startswith("Interface"):
                 port_line = line
                 break
         if not port_line:
