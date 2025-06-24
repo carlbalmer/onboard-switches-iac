3fe12ac8f30f39a1a10db2da2b910eb5f32a0b3a
Sacha Leemann <sacha.leemann@yahoo.de>
2025-06-24 16:53:53 +0200

    update interface

--- topologyDiscovery/StartDiscovery.py
+++ topologyDiscovery/StartDiscovery.py
@@ -2,19 +2,15 @@ from topologyDiscovery.KontorDiscovery import KontorDiscovery
 from topologyDiscovery.TopologyDiscovery import SystemInterface
 
 
-def get_system_interface(os_type: str) -> SystemInterface:
+def get_system_interface(system: str) -> SystemInterface:
     return KontorDiscovery()
 
 
 # Example usage
 if __name__ == "__main__":
-    import sys
-
-    # Determine the OS type (for demonstration purposes)
-    os_type = platform.system()  # This will return 'Linux', 'Windows', etc.
 
     # Get the appropriate system interface
-    system_interface = get_system_interface(os_type)
+    system_interface = get_system_interface("Kontor")
 
     # Use the interface
     print("System Info:", system_interface.getSystemInfo())
--- topologyDiscovery/TopologyDiscovery.py
+++ topologyDiscovery/TopologyDiscovery.py
@@ -1,14 +1,14 @@
-# write discovery script here
+from abc import ABC, abstractmethod
+# Interface Class that defines the must have methods for all implementations
+class SystemInterface(ABC):
+    @abstractmethod
+    def getSystemInfo(self):
+        pass
 
+    @abstractmethod
+    def discoverNeighbors(self, command: str):
+        pass
 
-def parseOutputForIp(output):
-    print("output parser")
-
-def executeCommand():
-    print("execute command")
-
-def getSystemInfo():
-    print("System is: x")
-
-def setSystemCategory():
-    print("System chose ist")
+    @abstractmethod
+    def parseOutput(self, output: str):
+        pass
