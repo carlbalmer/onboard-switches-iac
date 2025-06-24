e02e8844db27d6c3f6466a559bd801ca7e6351e8
Patrick Rauscher <prauscher@prauscher.de>
2025-06-24 11:10:49 +0200

    restructure

--- openrail/switchos/plugins/terminal/hirschmann.py
+++ openrail/switchos/plugins/terminal/hirschmann.py
@@ -1,6 +1,6 @@
 from ansible.plugins.terminal import TerminalBase
 
-class HirschmannTerminal(TerminalBase):
+class TerminalModule(TerminalBase):
     def __init__(self, connection):
         super().__init__(connection)
 
